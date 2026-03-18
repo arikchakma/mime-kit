83d2538b4e3bdbf1bfe463a8ef3001e0f85398bd
Arik Chakma <arikchangma@gmail.com>
2026-03-18 07:06:29 +0600

    feat: add public API re-exports from index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Headers } from './headers.ts';
+export { parse } from './parse.ts';
+export { build } from './build.ts';
+
+export type { Address, Email, ParsedAttachment } from './parse.ts';
+export type { AddressInput, AttachmentInput, BuildInput } from './build.ts';
